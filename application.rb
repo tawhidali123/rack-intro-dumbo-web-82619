927b5f642f2f41a6d987123013554fecb6a36f83
Tawhid Ali <tawhidali126@gmail.com>
2019-09-16 10:42:29 -0400

    added name

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World, my name is tawhid the great"
     resp.finish
   end
 
